505273691bc9ff230c90d1345116561b6755dbb8
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-03-06 13:47:12 +0530

    additing another list to test

---
+++ digital_root.py
@@ -0,0 +1,17 @@
+digit = 3081999
+"""" you have to find digit root for 15 is 1+5 = 6
+now to find the digit root if the length of the digit is not one then we need to add each digit upto it become one digit
+and to that single digit/number we call digit_root
+"""
+k = 0
+def root(n):
+    k= 0
+    for i  in str(n):
+        k +=int(i)
+    if len(str(k)) != 1:
+       return root(k)
+    else:
+        return k
+
+print(root(3081999))
+# print(b)
