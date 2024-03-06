b32ebcd516232bbeef53be4db6c318f2139b8bbe
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-03-06 13:47:29 +0530

    additing another list to test

--- digital_root.py
+++ digital_root.py
@@ -1,4 +1,3 @@
-digit = 3081999
 """" you have to find digit root for 15 is 1+5 = 6
 now to find the digit root if the length of the digit is not one then we need to add each digit upto it become one digit
 and to that single digit/number we call digit_root
