ac05d5af8c70588e30b70cc5ae1379fe8901a548
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-04-09 20:37:22 +0530

    web scrapping to get the product title in list

---
+++ web_scrapping.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+url = 'https://webscraper.io/test-sites/e-commerce/allinone'
+agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.1 Safari/605.1.15'
+HEADERS = ({'User-Agent': agent, 'Accept-Language': 'en-US, en;q=0.5'})
+
+webpage = requests.get(url, headers= HEADERS)
+soup = BeautifulSoup(webpage.content, 'html.parser')
+content = soup.find_all('a', attrs={'class': "title"})
+print(content)
+product_name =[]
+for i in range(len(content)):
+    product_name.append(content[i].text.strip().replace('...',''))
+print(product_name)
