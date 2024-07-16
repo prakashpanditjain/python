7a8100181fac0f49f2dfaa858c938ed170dc0e3f
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-07-16 14:32:38 +0530

    change in module

--- decorators.py
+++ decorators.py
@@ -1,25 +1,62 @@
-from Moduleimport_and_specialVariable__name__.addsub_module import add
+# from Moduleimport_and_specialVariable__name__.addsub_module import add
+# from Moduleimport_and_specialVariable__name__.addsub_module import sub
+#
+#
+# def outer(x):
+#     def innerfunction(y):
+#         if y > x:
+#             result = add(x, y)
+#         else:
+#             result = sub(x, y)
+#         return result
+#
+#     return innerfunction
+#
+#
+# def passfunc(x, y):
+#     r = outer(x)
+#     return r(y)
+#
+#
+# # add_five = outer(5)
+# # result = add_five(25)
+# # print(result)
+# if __name__ == '__main__':
+#     result = passfunc(5, 7)
+#     print(result)
+#
+#
+#
+# # USE OF DECORATOR
+# def make_pretty(fun):
+#     def inner():
+#         print("I got decorated")
+#         fun()
+#
+#     return inner
+#
+#
+# @make_pretty
+# def ordinary():
+#     print("I am ordinary")
+#
+# ordinary()
 
 
-def outer(x):
-    def innerfunction(y):
-        if y > x:
-            return add(x, y)
-        else:
-            return (x - y)
 
-    return innerfunction
+#USE OF __CLASS__() FUNCTION
 
+class Multiplier:
+    def __init__(self, factor):
+        self.factor = factor
 
-def passfunc(x, y):
-    r = outer(x)
-    return r(y)
+    def __call__(self, x):
+        return x * self.factor
 
+# Create an instance of Multiplier
+double = Multiplier(2)
+triple = Multiplier(3)
 
-# add_five = outer(5)
-# result = add_five(25)
-# print(result)
-if __name__ == '__main__':
-
-    result = passfunc(5, 7)
-    print(result)
+# Call the instances
+print(double(5))  # Output: 10
+print(triple(5))  # Output: 15
