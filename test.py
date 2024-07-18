e73ce4ac447b8ac6131aa0daa21a8cb13c5e6b87
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-07-18 08:49:51 +0530

    add requirement.txt

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-room_number = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 8]
-# vvuvcydr = aesjfasid
-
-for i in set(room_number):
-    room_number.remove(i)
-    if i not in room_number:
-        print(i)
-
 # count = {}
 # for i in room_number:
 #     if i in count:
@@ -18,3 +10,47 @@ for i in set(room_number):
 #     # print(key,value)
 #     if value != 5:
 #         print(key)
+
+"""
+Use of *args and **kwargs
+"""
+
+
+def function1(name, *args):
+    age = args[0]
+    profession = args[1]
+    print(f"my name is {name} and my age is {age} . I work as a {profession}")
+
+
+def keyword(*args, **kwargs):
+    for i in args:
+        # print(i)
+        # for key, value in kwargs.items():
+        # print("".join(f"{key}: {value}"))
+        # print('\n')
+
+        print(", ".join(f"{key}: {value}" for key, value in i.items()))
+
+
+# Passing the arguments to function1 method
+
+# function1("prakash", 28, "software engineer!")
+
+
+# defining the list which takes as * args
+people = [{"name": "Alice", "age": 28}
+    , {"name": "Bob", "age": 34},
+          {"name": "Catherine", "age": 22},
+          {"name": "David", "age": 40},
+          {"name": "Eva", "age": 31},
+          {"name": "Frank", "age": 25},
+          {"name": "Grace", "age": 29},
+          {"name": "Henry", "age": 37},
+          {"name": "Isabella", "age": 19},
+          {"name": "Jack", "age": 45}]
+
+keyword(*people)
+
+import pandas as pd
+
+print(pd)
