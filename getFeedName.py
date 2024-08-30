0d88ea391e89b1fd160f4809feab9cecd60318c0
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-08-30 09:51:46 +0530

    get feed name from URL

--- getFeedName.py
+++ getFeedName.py
@@ -1,15 +1,17 @@
 import os
+from urllib.parse import ParseResult
+from urllib.parse import ParseResultBytes
 from urllib.parse import urlparse
 
 from dotenv import load_dotenv
 
-def extract_feed_name(url):
-    parsed_url = urlparse(url)
+def extract_feed_name(url: object) -> object:
+    parsed_url: ParseResult | ParseResultBytes = urlparse(url)
     return parsed_url.netloc.replace("www.", "")
 
 
 if __name__ == '__main__':
     load_dotenv()
-    url = os.getenv('harry_url')
-    feedname = extract_feed_name(url)
+    url: str | None = os.getenv('harry_url')
+    feedname: str = extract_feed_name(url)
     print(feedname)
