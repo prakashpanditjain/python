871325320acd4bf7684f6a3662173324b297b528
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-11-05 05:29:43 +0530

    generator use case whilst file reading

---
+++ generator/filereading_generator_usecase.py
@@ -0,0 +1,29 @@
+"""
+Here’s a practical use case where a generator is highly efficient: reading and processing large files line-by-line.
+This is useful for scenarios where you need to process very large files that can’t fit into memory all at once.
+
+Use Case: Processing a Large Log File
+
+Imagine you have a large log file, access_log.txt, containing millions of lines of server access logs. Each line
+contains information like IP address, timestamp, and the requested resource. You need to identify and print lines
+that contain a specific IP address, but loading the entire file into memory is impractical.
+
+Solution with a Generator
+
+Using a generator allows you to read and process the file one line at a time, making it memory-efficient.
+"""
+
+
+def find_variable_in_file(file_path, variable):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if variable.strip() in line:
+                yield line
+
+
+file_path = "file_path"
+variable = "     ing      "
+
+# find_variable_in_file(file_path, variable)
+for xcom in find_variable_in_file(file_path, variable):
+    print(xcom)
