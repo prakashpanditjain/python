e1b32d68cb3607afc77df0c15ad3007d2c727ca0
ppandit8888@gmail.com <ppandit8888@gmail.com>
2024-12-04 22:01:26 +0530

    Number guessing beginner level python project

--- project/number_guessing.py
+++ project/number_guessing.py
@@ -11,23 +11,32 @@ guess_counter = 0
 chances = 8
 
 while guess_counter < chances:
-    # take a input from the user about the guess
+    # take an input from the user about the guess
     guess_counter += 1
 
     guess_number = int(input("Enter your number: "))
 
+    number_dict = {
+        1: 'st',
+        2: 'nd',
+        3: 'rd',
+        4: 'th',
+        5: 'th',
+        6: 'th',
+        7: 'th',
+        8: 'th',
+    }
+
     if guess_number == number_to_guess:
         print(f"Congratulations!! You have got a right number : {guess_number}")
-        if guess_number == 1:
-            print(f"On your {guess_counter}st chance")
-        if guess_number == 2:
-            print(f"On your {guess_counter}nd chance")
-        if guess_number == 4:
-            print(f"On your {guess_counter}rd chance")
-        else:
-            print(f"On your {guess_counter}th chance")
 
+        for key, value in number_dict.items():
+            if guess_counter == key:
+                print(f"on your {key}{value} chance")
+                if guess_counter >= 7:
+                    print("Bach gaya sale :>")
         break
+
     elif guess_counter >= chances and guess_number != number_to_guess:
         print(f"You have exhausted all {chances} chances , Better luck next time!!")
 
@@ -36,4 +45,3 @@ while guess_counter < chances:
 
     elif guess_number > number_to_guess:
         print("You are higher side")
-
