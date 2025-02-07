78f0f78c36925b2f295eb2b395ebe809a3cec4bc
ppandit8888@gmail.com <ppandit8888@gmail.com>
2025-02-07 16:07:33 +0530

    read and write file

--- 100_days_of_code_python/read_write_file/main.py
+++ 100_days_of_code_python/read_write_file/main.py
@@ -1,10 +1,18 @@
+import os
+
+file_path = os.path.realpath('my_file.txt')
+print(file_path)
 
 
-with open('my_file.txt.py') as file:
+with open('') as file:
     contents = file.read()
     print(contents)
-
-
-with open('new_file.txt',mode='r') as file:
-    contents = file.write("\n writing is in append mode")
-    print(contents)
+#
+#
+# with open('new_file.txt',mode='w') as file:
+#     contents = file.write("writing in append mode")
+#     print(contents)
+#
+#
+# print(os.path.realpath('new_file.txt'))
+# print(os.path.dirname('new_file.txt'))
