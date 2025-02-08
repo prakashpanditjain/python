8cbc74f44bfc597ef5bd2d8b4453453ab19c3e10
ppandit8888@gmail.com <ppandit8888@gmail.com>
2025-02-08 15:46:57 +0530

    working with pandas (count)

---
+++ 100_days_of_code_python/csv_and_pandas/main.py
@@ -0,0 +1,25 @@
+import pandas
+from pandas.core.interchange.dataframe_protocol import DataFrame
+
+from generator.generator import count
+
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+#
+
+# gray_data = len(data[data['Primary Fur Color'] == 'Gray'])
+# cinnamon_data = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+# black_data = len(data[data['Primary Fur Color'] == 'Black'])
+# # data = data[]
+#
+# # print(data[data.groupby('Primary Fur Color')].count('Unique Squirrel ID'))
+# print(gray_data)
+# data_dict = {
+#     'fur_color': ['Gray','cinnamon','black'],
+#     'count' : [gray_data,cinnamon_data,black_data]
+# }
+
+
+df = data.groupby(['Primary Fur Color'])['Unique Squirrel ID'].count()
+print(df)
+
+df.to_csv('sqirrel_count')
