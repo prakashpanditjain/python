9a258ad1a0b1840664f7dc59a6b37751bae4a1fe
ppandit8888@gmail.com <ppandit8888@gmail.com>
2025-03-07 18:14:57 +0530

    stock market alert for change in daily closing price

--- 100_days_of_code_python/stock-news-extrahard-start/main.py
+++ 100_days_of_code_python/stock-news-extrahard-start/main.py
@@ -64,18 +64,3 @@ while NEED_OF_NEWS:
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
 # by connecting it to twilio API we can ssend msg to mobile number using their template
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to 
-file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height 
-of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to 
-file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height 
-of the coronavirus market crash.
-"""
