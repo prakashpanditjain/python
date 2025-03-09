790d404510a056f835cb76eb8fd1e637d00c73c7
ppandit8888@gmail.com <ppandit8888@gmail.com>
2025-03-09 16:04:27 +0530

    Add new HTML files and images for various elements in the 100 Days of Code project

---
+++ 100_days_of_code_python/workout_tracking_using_googlesheet/main.py
@@ -0,0 +1,9 @@
+nutritionix_endpoint = " trackapi.nutritionix.com/v2"
+api_key = ""
+appid = ""
+
+headers = {
+    "x-app-id" : appid,
+    "x-app-key": api_key,
+}
+# Go to day 38 of the 100 days of code
