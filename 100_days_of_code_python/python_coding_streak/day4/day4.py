934e4a7c92e7a8789668cf4218ce8f3e3475badf
ppandit8888@gmail.com <ppandit8888@gmail.com>
2025-12-18 23:56:16 +0530

    Add function to find the length of the longest substring without repeating characters

---
+++ 100_days_of_code_python/python_coding_streak/day4/day4.py
@@ -0,0 +1,35 @@
+"""
+Today we step into sliding window land, where efficiency stops being optional.
+
+Problem: Longest Substring Without Repeating Characters
+
+You’re given a string s.
+Find the length of the longest substring that contains no repeating characters.
+
+Example:
+Input: "abcabcbb"
+Output: 3 → "abc"
+
+Input: "bbbbb"
+Output: 1 → "b"
+
+Input: "pwwkew"
+Output: 3 → "wke"
+"""
+def longest_substring_without_repeating_characters(s:str )-> int:
+    char_index_map =  {}
+    left_pointer = 0
+    max_length = 0
+
+    for index,char in enumerate(s):
+        if char in char_index_map and char_index_map[char] > left_pointer:
+            left_pointer = char_index_map[char] +1
+            char_index_map[char] = index
+
+        else:
+            char_index_map[char] = index
+        max_length = max(max_length, index - left_pointer +1)
+    return max_length
+
+lswrc = longest_substring_without_repeating_characters('abcdefghijkldmnopqrstuvwxycdeakufasebr')
+print(lswrc)
