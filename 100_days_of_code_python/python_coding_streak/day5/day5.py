de3d997d7774e80c2100445ed616e4c20e0036e8
ppandit8888@gmail.com <ppandit8888@gmail.com>
2025-12-23 23:31:04 +0530

    Add function to sort characters by frequency in a string

--- 100_days_of_code_python/python_coding_streak/day5/day5.py
+++ 100_days_of_code_python/python_coding_streak/day5/day5.py
@@ -18,14 +18,14 @@ Output: [1]
 from sqlalchemy.orm.sync import bulk_populate_inherit_keys
 
 
-def top_k_frequent_elements(nums: list[int], k: int) -> list[int]:
+def top_k_frequent_elements(s: str, k: int) -> list[int]:
     frequency_map = {}
-    for num in nums:
-        frequency_map[num] = frequency_map.get(num, 0) + 1
+    for char in s:
+        frequency_map[char] = frequency_map.get(char, 0) + 1
 
     sorted_tuple = sorted(frequency_map.items(), key= lambda x : x[1], reverse=True)
     result = [num for num, freq in sorted_tuple[:k]]
     return result
 
-tkfe = top_k_frequent_elements([1,1,1,3,2,4,2,2,3,3,3,3,3,2,1,4,5,6,7,5,3,4,5],4)
+tkfe = top_k_frequent_elements('mississippy',2)
 print(tkfe)
