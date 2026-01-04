3c4422139f91054d7b0a72e24de800b6468648d3
ppandit8888@gmail.com <ppandit8888@gmail.com>
2026-01-04 15:42:23 +0530

    Add function to determine if two strings are isomorphic

---
+++ 100_days_of_code_python/python_coding_streak/day8/day8.py
@@ -0,0 +1,9 @@
+"""
+Given two strings s and t, determine if they are isomorphic. Two strings are isomorphic if the characters in s can be replaced to get t.
+
+"""
+
+
+
+def is_isomorphic(s: str, t: str) -> bool:
+    # your code
