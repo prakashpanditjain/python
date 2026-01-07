882dfdbf350eab41768821b67a49f246bea221c8
ppandit8888@gmail.com <ppandit8888@gmail.com>
2026-01-07 10:14:08 +0530

    Add function to count subarrays with sum equal to zero

---
+++ 100_days_of_code_python/python_coding_streak/day13/day13.py
@@ -0,0 +1,29 @@
+"""
+Count Subarrays with Sum = 0
+
+Given an array of integers (can include negatives), return the number of subarrays whose sum is 0.
+
+Example:
+[1, -1, 2, -2, 3]
+Output: 3
+
+This is the same pattern you just learned, stripped to its bones.
+"""
+
+def count_zero_sum_subarrays(nums: list[int]) -> int:
+    count = 0
+    current_sum = 0
+    sum_frequency = {0: 1}
+
+    for num in nums:
+        current_sum += num  #1
+        # if current_sum == 0:
+        #     count += sum_frequency[0]
+        if current_sum in sum_frequency:
+            count += sum_frequency[current_sum]
+        sum_frequency[current_sum] = sum_frequency.get(current_sum, 0) + 1
+
+    return count
+
+czss = count_zero_sum_subarrays( [0,0,0] )
+print(czss)
